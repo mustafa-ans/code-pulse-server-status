0edb9e0def19aed1c35599795a5d398e1052806d
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-08-19 23:43:26 +0530

    added icon in status bar to run custom command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem.text = "$(play) Run Cargo Watch";
+    statusBarItem.command = 'extension.runCargoWatch';
+    statusBarItem.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-pulse-server-status" is now active!');
+    const disposable = vscode.commands.registerCommand('extension.runCargoWatch', () => {
+        const terminal = vscode.window.createTerminal('Cargo Watch');
+        terminal.sendText('cargo watch -x run');
+        terminal.show();
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-pulse-server-status.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Pulse!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(statusBarItem, disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
