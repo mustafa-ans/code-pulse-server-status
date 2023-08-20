7f1f695ea5319856038eb4e09c92d78c13a15564
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-08-21 00:02:50 +0530

    iteration one, runs the command onclick button

--- src/extension.js
+++ src/extension.js
@@ -1,16 +1,16 @@
-import * as vscode from 'vscode';
-
-let statusBarItem: vscode.StatusBarItem;
-let isExtensionRunning: boolean = false; // Track whether the extension is running
-
-export function activate(context: vscode.ExtensionContext) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
+let statusBarItem;
+let isExtensionRunning = false; // Track whether the extension is running
+function activate(context) {
     // Create a status bar item with the "play-circle" icon initially
     statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
     statusBarItem.text = "$(play-circle)";
     statusBarItem.tooltip = "Click to run cargo watch -x run";
     statusBarItem.command = 'extension.runCargoWatch';
     statusBarItem.show();
-
     // Register the command to run Cargo Watch
     const disposable = vscode.commands.registerCommand('extension.runCargoWatch', () => {
         isExtensionRunning = true; // Extension is now in use
@@ -20,24 +20,24 @@ export function activate(context: vscode.ExtensionContext) {
         terminal.sendText('cargo watch -x run');
         terminal.show();
     });
-
     // Add the disposable to the context's subscriptions
     context.subscriptions.push(statusBarItem, disposable);
 }
-
-export function deactivate() {
+exports.activate = activate;
+function deactivate() {
     // Dispose the status bar item when the extension is deactivated
     statusBarItem.dispose();
 }
-
+exports.deactivate = deactivate;
 // Function to update the status bar icon based on the extension state
 function updateStatusBarIcon() {
     if (isExtensionRunning) {
         statusBarItem.text = "$(pulse)"; // Change icon to "pulse"
-    } else {
+    }
+    else {
         statusBarItem.text = "$(play-circle)"; // Change icon to "play-circle"
     }
 }
-
 // Register an interval to update the icon periodically (for example, every second)
 setInterval(updateStatusBarIcon, 1000); // You can adjust the interval as needed
+//# sourceMappingURL=extension.js.map
