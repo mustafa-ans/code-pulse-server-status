4dd3b2ed2b3d0e017dea640187d78a4c3f2cfccb
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-08-21 23:24:24 +0530

    added a script in JS to read terminal output

--- src/extension.js
+++ src/extension.js
@@ -2,8 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
+const path = require("path"); // Import path module
+const child_process_1 = require("child_process"); // Import fork for IPC
+let watcherProcess;
 let statusBarItem;
 let isExtensionRunning = false; // Track whether the extension is running
+let isServerRunning = false; // Track whether the server is running
 function activate(context) {
     // Create a status bar item with the "play-circle" icon initially
     statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
@@ -19,6 +23,16 @@ function activate(context) {
         const terminal = vscode.window.createTerminal('Cargo Watch');
         terminal.sendText('cargo watch -x run');
         terminal.show();
+        // Start the terminal output watcher
+        const terminalOutputFilePath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, 'terminal-output.txt');
+        // Use fork to spawn the terminalWatcher.js process for IPC
+        watcherProcess = (0, child_process_1.fork)('terminalWatcher.js', [terminalOutputFilePath]);
+        watcherProcess.on('message', (message) => {
+            if (message === 'server_running') {
+                isServerRunning = true;
+                updateStatusBarIcon();
+            }
+        });
     });
     // Add the disposable to the context's subscriptions
     context.subscriptions.push(statusBarItem, disposable);
@@ -27,16 +41,27 @@ exports.activate = activate;
 function deactivate() {
     // Dispose the status bar item when the extension is deactivated
     statusBarItem.dispose();
+    if (watcherProcess) {
+        watcherProcess.kill();
+    }
+    isServerRunning = false; // Reset server status
+    updateStatusBarIcon(); // Update the status bar icon immediately
 }
 exports.deactivate = deactivate;
 // Function to update the status bar icon based on the extension state
 function updateStatusBarIcon() {
     if (isExtensionRunning) {
-        statusBarItem.text = "$(pulse)"; // Change icon to "pulse"
+        if (isServerRunning) {
+            statusBarItem.text = "$(radio-tower)"; // Change icon to "radio-tower"
+        }
+        else {
+            statusBarItem.text = "$(pulse)"; // Change icon to "pulse"
+        }
     }
     else {
         statusBarItem.text = "$(play-circle)"; // Change icon to "play-circle"
     }
+    statusBarItem.show(); // Make sure to call show() after changing the text property
 }
 // Register an interval to update the icon periodically (for example, every second)
 setInterval(updateStatusBarIcon, 1000); // You can adjust the interval as needed
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
 import * as vscode from 'vscode';
+import * as path from 'path'; // Import path module
+import * as cp from 'child_process'; // Import child_process module
+
+import { ChildProcess, fork } from 'child_process'; // Import fork for IPC
+
+let watcherProcess: ChildProcess | undefined;
 
 let statusBarItem: vscode.StatusBarItem;
 let isExtensionRunning: boolean = false; // Track whether the extension is running
+let isServerRunning: boolean = false; // Track whether the server is running
 
 export function activate(context: vscode.ExtensionContext) {
     // Create a status bar item with the "play-circle" icon initially
@@ -19,6 +26,18 @@ export function activate(context: vscode.ExtensionContext) {
         const terminal = vscode.window.createTerminal('Cargo Watch');
         terminal.sendText('cargo watch -x run');
         terminal.show();
+        // Start the terminal output watcher
+        const terminalOutputFilePath = path.join(vscode.workspace.workspaceFolders![0].uri.fsPath,'terminal-output.txt');
+          
+        // Use fork to spawn the terminalWatcher.js process for IPC
+        watcherProcess = fork('terminalWatcher.js', [terminalOutputFilePath]);
+
+        watcherProcess.on('message', (message) => {
+            if (message === 'server_running') {
+                isServerRunning = true;
+                updateStatusBarIcon();
+            }
+        });
     });
 
     // Add the disposable to the context's subscriptions
@@ -28,16 +47,29 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
     // Dispose the status bar item when the extension is deactivated
     statusBarItem.dispose();
+    if (watcherProcess) {
+        watcherProcess.kill();
+    }
+    isServerRunning = false; // Reset server status
+    updateStatusBarIcon(); // Update the status bar icon immediately
 }
+
 
 // Function to update the status bar icon based on the extension state
 function updateStatusBarIcon() {
     if (isExtensionRunning) {
-        statusBarItem.text = "$(pulse)"; // Change icon to "pulse"
+        if (isServerRunning) {
+            statusBarItem.text = "$(radio-tower)"; // Change icon to "radio-tower"
+        } else {
+            statusBarItem.text = "$(pulse)"; // Change icon to "pulse"
+        }
     } else {
         statusBarItem.text = "$(play-circle)"; // Change icon to "play-circle"
     }
+    
+    statusBarItem.show(); // Make sure to call show() after changing the text property
 }
+
 
 // Register an interval to update the icon periodically (for example, every second)
 setInterval(updateStatusBarIcon, 1000); // You can adjust the interval as needed
